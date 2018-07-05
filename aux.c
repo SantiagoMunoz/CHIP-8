b935926a468c2b197a4e72bdad4327901c7653d9
Santiago Munoz <smr.rambar@gmail.com>
2018-07-05 11:13:12 +0200

    More elegant ascii hex to byte conversion

--- aux.c
+++ aux.c
@@ -2,42 +2,10 @@
 
 unsigned char char2byte(char input){
 
-    switch(input){
-        case '0':
-            return 0;
-        case '1':
-            return 1;
-        case '2':
-            return 2;
-        case '3':
-            return 3;
-        case '4':
-            return 4;
-        case '5':
-            return 5;
-        case '6':
-            return 6;
-        case '7':
-            return 7;
-        case '8':
-            return 8;
-        case '9':
-            return 9;
-        case 'A':
-            return 10;
-        case 'B':
-            return 11;
-        case 'C':
-            return 12;
-        case 'D':
-            return 13;
-        case 'E':
-            return 14;
-        case 'F':
-            return 15;
-        default:
-            printf("Warning! Processing illegal hexadecimal digit.\n");
-    }
-    return 0;
+	if((input >= '0') & (input <= '9'))
+		return input-'0';
+	if((input >= 'A') & (input <= 'F'))
+		return input-'A'+10;
+	printf("Warning! Processing illegal hexadecimal digit.\n");
+	return 0;
 }
-
