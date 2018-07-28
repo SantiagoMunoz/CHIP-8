40281138b67d2668b14cb8b4336ca37a28f61bba
Santiago Munoz <smr.rambar@gmail.com>
2018-07-28 19:13:11 +0200

    Exposed match_opcode in c8Instructions.h

--- c8Instructions.h
+++ c8Instructions.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 
 typedef void(*c8Func)(c8Env*,uint16_t);
+uint8_t match_opcode(uint16_t opcode, uint16_t reg, uint16_t mask);
 
 void c8_CLS(c8Env *env, uint16_t opcode);
 void c8_RET(c8Env *env, uint16_t opcode); 
